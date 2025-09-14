8e212b4fe08489a3a2fb1c465b593798967a4bcf
WilliamNyamu <williamnyamu08@gmail.com>
2025-09-14 09:56:32 +0300

    first commit

---
+++ polls/models.py
@@ -0,0 +1,28 @@
+import datetime
+from django.db import models
+# models - essentially a database layout, with additional metadata
+# Create your models here.
+
+class Question(models.Model):
+    question_text = models.CharField(max_length=200)
+    pub_date = models.DateTimeField("date published")
+
+    def __str__(self):
+        return self.question_text
+    
+    def was_published_recently(self):
+        return self.pub_date >= datetime.datetime.now(datetime.timezone.utc) - datetime.timedelta(days=1)
+
+class Choice(models.Model):
+    question = models.ForeignKey(Question, on_delete=models.CASCADE)
+    choice_text = models.CharField(max_length=200)
+    votes = models.IntegerField(default=0)
+
+    def __str__(self):
+        return self.choice_text
+
+
+
+# username: admin
+# email: admin@example.com
+# password: admin
